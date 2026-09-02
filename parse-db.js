9c17b53de559b3063939d44dba6efc0faa7d93cc
Pruthviraj141 <pruthvigavhane@gmail.com>
2026-09-02 09:31:03 +0530

    chore: clean up root directory by removing scratchpads and temp files

--- parse-db.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const services = JSON.parse(fs.readFileSync('/tmp/services.json', 'utf8')).services || [];
-const camps = JSON.parse(fs.readFileSync('/tmp/camps.json', 'utf8')).camps || [];
-
-console.log("=== SERVICES ===");
-services.forEach(s => console.log(`- [${s.type.toUpperCase()}] ${s.name} in ${s.city}`));
-console.log("\n=== CAMPS (SHELTERS) ===");
-camps.forEach(c => console.log(`- [${c.type.toUpperCase()}] ${c.name} in ${c.city}`));
